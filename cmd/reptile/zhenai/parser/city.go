177ef1dd6156872aa7efd3cd921099a14e70e7d6
Junxiong.Xu <Junxiong.Xu@geely.com>
2019-09-02 21:27:03 +0800

    add city parse

---
+++ cmd/reptile/zhenai/parser/city.go
@@ -0,0 +1,27 @@
+package parser
+
+import (
+	"go_mod-project/cmd/reptile/engine"
+	"regexp"
+)
+
+// TODO: 可能是错误的正则
+const cityRe = `<a href="(http://album.zhenai.com/zhenghun/u/[0-9]+)"[^>]*>([^<]+)</a>`
+
+func ParseCity(contents []byte) engine.ParserResult  {
+	re := regexp.MustCompile(cityListRe)
+	matches := re.FindAllSubmatch(contents, -1)
+
+	result := engine.ParserResult{}
+	for _, m := range matches {
+		name := string(m[2])
+		result.Items = append(result.Items, "User " + string(m[2]))
+		result.Requests = append(result.Requests, engine.Request{
+			Url:        string(m[1]),
+			ParserFunc: func(c []byte) engine.ParserResult {
+				return ParseProfile(c, name)
+			},
+		})
+	}
+	return result
+}
---
+++ cmd/reptile/zhenai/parser/profile.go
@@ -0,0 +1,39 @@
+package parser
+
+import (
+	"go_mod-project/cmd/reptile/engine"
+	"go_mod-project/cmd/reptile/models"
+	"regexp"
+	"strconv"
+)
+
+var ageRe = regexp.MustCompile(`<td><span class="label">年龄: </span>([\d]+)岁</td>`)
+var marriageRe = regexp.MustCompile(`<td><span class="label">婚况: </span>([^<]+)</td>`)
+
+
+func ParseProfile(contents []byte, name string) engine.ParserResult  {
+	profile := models.Profile{}
+	profile.Name = name
+
+	age, err := strconv.Atoi(extractString(contents, ageRe))
+	if err != nil {
+		profile.Age = age
+	}
+
+	profile.Marriage = extractString(contents, marriageRe)
+
+
+	result := engine.ParserResult{
+		Items:    []interface{}{profile},
+	}
+	return result
+}
+
+func extractString(contents []byte, re *regexp.Regexp) string {
+	match := re.FindSubmatch(contents)
+	if len(match) >= 2 {
+		return string(match[1])
+	} else {
+		return ""
+	}
+}
